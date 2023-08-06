f2fea2ba71945b20a86df74f6026fabb2f61b6ca
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-05 23:57:46 -0500

    Create Teatro.java

---
+++ src/Logica/Teatro.java
@@ -0,0 +1,12 @@
+public class Teatro extends InteresCultural {
+    @Override
+    public String getNombre() {
+        return "Teatro";
+    }
+    
+    @Override
+    public String getDescripcion() {
+        return "Un teatro es un lugar donde se representan obras y espectáculos teatrales.";
+    }
+    // Implementación específica para teatros
+}
