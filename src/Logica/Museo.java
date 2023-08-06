ac0dfa243d1d23ff626337a94abde72d5e8cd68b
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-05 23:57:11 -0500

    Create Museo.java

---
+++ src/Logica/Museo.java
@@ -0,0 +1,12 @@
+public class Museo extends InteresCultural {
+    @Override
+    public String getNombre() {
+        return "Museo";
+    }
+    
+    @Override
+    public String getDescripcion() {
+        return "Un museo es un lugar donde se exhiben y conservan objetos de valor cultural, artístico o histórico.";
+    }
+    // Implementación específica para museos
+}
