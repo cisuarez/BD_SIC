cd21f85620cbbfefe615c389e2b360045f742551
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-06 00:24:44 -0500

    Create dMunicipios.java

---
+++ src/Logica/dMunicipios.java
@@ -0,0 +1,17 @@
+public class dMunicipios {
+    private String id_municipio;
+    private String nombre_municipio;
+    private String ubicacion_municipio;
+    private String clima_municipio;
+    private FileInputStream escudo_municipio;
+    private String id_region_municipio;
+    private int numero_habitantes;
+    private int numero_mujeres;
+    private int numero_hombre;
+
+    // Constructor privado para evitar instancias directas
+    private dMunicipios() {}
+
+    // Getters y setters para los atributos
+    // ...
+}
