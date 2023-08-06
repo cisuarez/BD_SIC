85bf1cbc0e5761df2ef75f9e1a7c6fcb10119406
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-05 23:54:32 -0500

    Create abstract class InteresCultural.java

---
+++ src/Logica/InteresCultural.java
@@ -0,0 +1,5 @@
+public abstract class InteresCultural {
+    public abstract String getNombre();
+    public abstract String getDescripcion();
+    // Otros métodos relevantes para intereses culturales
+}
